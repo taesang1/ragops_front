d8758a2f8ff2c5ec5f6e8e80711c24b94c55ebba
taesang <tsjung@intellisys.co.kr>
2024-09-24 20:28:42 +0900

    rollback

--- pages/project.vue
+++ pages/project.vue
@@ -7,19 +7,28 @@
           <tr>
             <th>프로젝트</th>
             <th>생성 시작 시간</th>
+            <th>상태</th>
+            <th>옵션</th>
+            <th>청킹 결과</th>
             <th>생성 완료 시각</th>
+            <th>데이터 갱신</th>
+            <th>테스트</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>프로젝트 1</td>
+            <td>1</td>
             <td>2024-09-23 12:00</td>
+            <td>생성완료</td>
+            <td><a>보기</a></td>
+            <td><a>보기</a></td>
             <td>2024-09-23 12:12</td>
+            <td><button class="table-button">데이터 갱신</button></td>
+            <td><button @click="test" class="table-button">테스트</button></td>
           </tr>
         </tbody>
       </table>
     </div>
-    <button style="float: right;" class="nuxt-button">프로젝트 추가 ></button>
   </div>
 </template>
 <script>
